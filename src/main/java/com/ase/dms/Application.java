18be8f5a43cd43a1e28e1ee7fa4fbadeb7949929
Ole <ole.lemnitzer@gmail.com>
2025-09-04 11:40:37 +0200

    Vorbereitung auf Einbindung Abstract Files Service

--- src/main/java/com/ase/dms/Application.java
+++ src/main/java/com/ase/dms/Application.java
@@ -1,11 +1,20 @@
 package com.ase.dms;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition(
+    info = @Info(title = "DMS API", version = "1.0"),
+    servers = {
+        @Server(url = "${API_BASE_URL:http://localhost:8080}", description = "API Server"),
+    }
+)
 public class Application {
 
   public static void main(String[] args) {
