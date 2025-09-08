a89e321d02cc0ec4df6521549d70da10656b2493
Ole <ole.lemnitzer@gmail.com>
2025-09-08 08:25:15 +0200

    Fix linter

--- src/main/java/com/ase/dms/controllers/DocumentsController.java
+++ src/main/java/com/ase/dms/controllers/DocumentsController.java
@@ -2,10 +2,9 @@ package com.ase.dms.controllers;
 
 import com.ase.dms.entities.DocumentEntity;
 import com.ase.dms.services.DocumentService;
-import io.swagger.v3.oas.annotations.Operation;
+
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -27,8 +26,7 @@ public class DocumentsController {
     return ResponseEntity.ok(documentService.getDocument(id));
   }
 
-  // <-- NEU: kein {id} im Pfad, folderId als RequestParam
-  @PostMapping
+  @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public ResponseEntity<DocumentEntity> uploadDocument(
       @Parameter(description = "File to upload", required = true) @RequestParam("file") MultipartFile file,
       @Parameter(description = "Target folder UUID", required = true) @RequestParam("folderId") String folderId) {
