b1e03daa96d36201e5d2113f033da879ade3b2f3
noahkemmesies <81417115+noahkemmesies@users.noreply.github.com>
2025-10-16 14:34:19 +0200

    Testing Postgres DB Connection, added Checker for DB Password length (#11)
    * Testing Postgres DB Connection, added Checker for DB Password length
    
    * Updated default DB Password

---
+++ src/main/java/com/ase/dms/config/DbConfigChecker.java
@@ -0,0 +1,22 @@
+package com.ase.dms.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DbConfigChecker {
+
+  @Value("${spring.datasource.password}")
+  private String dbPassword;
+
+  @PostConstruct
+  public void checkPassword() {
+    if (dbPassword != null) {
+      System.out.println("DB Password length: " + dbPassword.length());
+    }
+    else {
+      System.out.println("DB Password ist null");
+    }
+  }
+}
