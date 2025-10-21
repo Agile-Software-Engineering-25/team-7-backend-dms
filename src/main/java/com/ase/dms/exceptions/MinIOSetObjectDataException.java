07a12fbd0a83327ec4e889530d98a85e3e1c3c3c
Sven Boerrigter <sven.boerrigter@stud-provadis-hochschule.de>
2025-10-21 11:27:52 +0200

    set error message

--- src/main/java/com/ase/dms/exceptions/MinIOSetObjectDataException.java
+++ src/main/java/com/ase/dms/exceptions/MinIOSetObjectDataException.java
@@ -4,7 +4,7 @@ public class MinIOSetObjectDataException extends DmsException {
 
   public MinIOSetObjectDataException(String documentId) {
     super(ErrorCodes.SYS_INTERNAL_ERROR,
-        "Document with ID '" + documentId + "' could not be retrieved.");
+        "Document with ID '" + documentId + "' could not be uploaded.");
   }
 
   public MinIOSetObjectDataException(String documentId, Throwable cause) {
