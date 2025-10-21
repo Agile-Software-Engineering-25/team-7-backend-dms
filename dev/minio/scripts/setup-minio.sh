6cac4b754673261d4dd1d6fc920aff3f5d33d844
Sven Boerrigter <sven.boerrigter@stud-provadis-hochschule.de>
2025-10-21 11:16:21 +0200

    wip: testing setup

--- dev/minio/scripts/setup-minio.sh
+++ dev/minio/scripts/setup-minio.sh
@@ -9,10 +9,10 @@ done
 
 echo "MinIO is ready! Setting up users, policies, and buckets..."
 
-# Create buckets for different microservices (with idempotency)
+# Create buckets
 echo "Creating buckets..."
-mc mb minio/ase-team-7 --ignore-existing
-mc mb minio/shared-bucket --ignore-existing
+mc mb minio/test --ignore-existing
+mc mb minio/test-shared-bucket --ignore-existing
 
 # Function to create a policy from a file in the policies folder
 create_policy() {
@@ -34,36 +34,10 @@ for policy_path in /policies/*.json; do
         create_policy "$policy_name"
 done
 
-# Function to create a user from JSON file and env variables
-# Takes a JSON file with "username" and "policy" fields
-# Maps the username as an env variable to get the actual username and password
-# E.g. for username "file-service", it looks for env vars MINIO_FILE_SERVICE_USER and MINIO_FILE_SERVICE_PASSWORD
-# Creates the user and attaches the policy
-create_user_from_json() {
-        local json_file="$1"
-        local user_name=$(jq -r '.username' "$json_file")
-        local policy_name=$(jq -r '.policy' "$json_file")
-        # Map username to env variable for password, e.g. MINIO_FILE_SERVICE_USER -> MINIO_FILE_SERVICE_PASSWORD
-        local env_user_var="MINIO_$(echo "$user_name" | tr 'a-z-' 'A-Z_')_USER"
-        local env_password_var="MINIO_$(echo "$user_name" | tr 'a-z-' 'A-Z_')_PASSWORD"
-        local user_password="test"
-        echo "Creating user: $user_env_name"
-        mc admin user add minio "$user_name" "$user_password"
-        if [ -n "$policy_name" ] && [ "$policy_name" != "null" ]; then
-                echo "Assigning policy $policy_name to user $user_name"
-                mc admin policy attach minio "$policy_name" --user "$user_name"
-        else
-                echo "No policy specified for user $user__name, skipping policy assignment."
-        fi
-}
-
 # Create users from JSON files in /users folder
-echo "Creating users from /users JSON files..."
-for user_json in /users/*.json; do
-        if [ -f "$user_json" ]; then
-                create_user_from_json "$user_json"
-        fi
-done
+echo "Creating test user"
+mc admin user add minio "test" "testpassword"
+mc admin policy attach minio test --user "test"
 
 echo "Setup complete!"
 echo "==========================="
